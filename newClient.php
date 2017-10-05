b004fdfef537df5970d44cba73bdfd556d3c8311
Jonathan Nicholas <jonicrecis@gmail.com>
2017-10-05 22:53:10 +1100

    New Client implementation

---
+++ newClient.php
@@ -0,0 +1,49 @@
+<?php
+    ob_start();
+	
+	if (!($_SESSION["access_status"] === "granted")) {
+		$page = "insert_it.php";
+		header("location: login.php?Page=$page");
+	}
+
+?>
+
+<?php
+    include("connection.php");
+	$conn = new mysqli($HOST, $USERNAME, $PASSWORD, $DATABASE);
+    $query = "INSERT INTO client (client_fname,client_lname,client_street,client_suburb,client_state,client_pc,client_email,client_mobile,client_mailing_list) VALUES(?,?,?,?,?,?,?,?,?)";
+    $pquery = mysqli_prepare($conn,$query);
+    $pquery->bind_param('sssssssss',$fname,$lname,$street,$suburb,$state,$postcode,$email,$mobile,$mailinglist);
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $street = $_POST["addr"];
+    $suburb = $_POST["suburb"];
+    $state = $_POST["state"];
+    $postcode = $_POST["pcode"];
+    $email = $_POST["email"];
+    $mobile = $_POST["phonenum"];
+    if(empty($_POST["check"]))
+    {
+        $mailinglist = "N";
+    }
+    else
+    {
+        $mailinglist = "Y";
+    }
+    if ($pquery->execute()) {
+        ?>
+        <script language="JavaScript">
+            alert("New client successfully added to database!");
+        </script>
+        <?php
+        header("Location: single_product.php");
+    } else {
+        ?>
+        <script language="JavaScript">
+            alert("Error adding record. Contact System Administrator");
+        </script>
+        <?php
+        $result->free_result();
+        $conn->close();
+}
+    ?>
---
+++ newClient_form.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+if (!($_SESSION["access_status"] === "granted")) {
+    $page = "newClient_form.php";
+    header("location: login.php?Page=$page");
+}
+?>
+<html>
+<head><title>New Client Form</title></head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<form method="post" action="newClient.php">
+    <center>Client detail<br/></center>
+    <p/>
+    <table align="center" cellpadding="3">
+        <tr>
+            <td><b>First Name</b></td>
+            <td><input type="text" name="fname" size="30" value="" required></td>
+        </tr>
+        <tr>
+            <td><b>Last Name</b></td>
+            <td><input type="text" name="lname" size="30" value="" required></td>
+        </tr>
+        <tr>
+            <td><b>Address</b></td>
+            <td><input type="text" name="addr" size="30" value="" required></td>
+        </tr>
+        <tr>
+            <td><b>Suburb</b></td>
+            <td><input type="text" name="suburb" size="30" value="" required></td>
+        </tr>
+        <tr>
+            <td><b>State</b></td>
+            <td><input type="text" name="state" size="30" value="" required></td>
+        </tr>
+        <tr>
+            <td><b>Postcode</b></td>
+            <td><input type="text" name="pcode" size="30" value="" required></td>
+        </tr>
+        <tr>
+            <td><b>Email</b></td>
+            <td><input type="text" name="email" size="30" value="" required></td>
+        </tr>
+        <tr>
+            <td><b>Phone Number</b></td>
+            <td><input type="number" name="phonenum" size="30" value="" required></td>
+        </tr>
+        <tr>
+            <td><b>Mailing List</b></td>
+            <td><input type="checkbox" name="check[]" size="30"></td>
+        </tr>
+    </table>
+    <br/>
+    <table align="center">
+        <br/>
+        <tr>
+            <td><input type="submit" value="Insert product"></td>
+            <td><input type="button" value="return to list" OnClick="window.location='single_product.php'"></td>
+        </tr>
+    </table>
+</form>
+
+</body>
+
+</html>
